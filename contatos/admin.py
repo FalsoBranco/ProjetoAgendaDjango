fbd052959a18612059448b0f551540028ee537ff
helter daniel <helterdaniel@hotmail.com>
2021-04-05 20:39:27 -0300

    modelo contato e base index

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from contatos import models
+
+from .models import Categoria, Contato
+
 # Register your models here.
+
+
+@admin.register(Contato)
+class ContatoAdmin(admin.ModelAdmin):
+    pass
+    
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    pass
+
--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField("nome categoria", max_length=50)
+    def __str__(self) -> str:
+        return self.nome
+
+class Contato(models.Model):
+    nome = models.CharField("nome contato", max_length=255)
+    sobrenome = models.CharField("sobrenome contato",max_length=255, blank=True)
+    telefone = models.CharField("telefone contato",max_length=13)
+    email = models.CharField("email contato", max_length=254,blank=True)
+    data_criacao = models.DateTimeField("data criacao contato", auto_now=True,)
+    descricao = models.TextField("descricao contato",blank=True)
+    categoria = models.ForeignKey("contatos.Categoria", verbose_name="categoria contato", on_delete=models.DO_NOTHING)
+    
+    
+
+    class Meta:
+        verbose_name = ("contato")
+        verbose_name_plural = ("contatos")
+
+    def __str__(self):
+        return self.nome    
+
+
+
