ed13018478e41a4851bc5a72ba0f74457c1715b5
helter daniel <helterdaniel@hotmail.com>
2021-04-07 21:17:46 -0300

    adicionado pasta de templates

--- core/settings.py
+++ core/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -74,22 +74,22 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "agenda",
-        "USER": "admin",
-        "PASSWORD": "root",
-        "HOST": "mysqldb",  # Or an IP Address that your DB is hosted on
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "agenda",
+#         "USER": "admin",
+#         "PASSWORD": "root",
+#         "HOST": "mysqldb",  # Or an IP Address that your DB is hosted on
+#         "PORT": "3306",
+#     }
+# }
 
 
 # Password validation
@@ -129,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "templates/static"]
