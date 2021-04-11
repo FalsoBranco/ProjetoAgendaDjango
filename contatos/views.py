d47f6173f82e983868f062c754a3f193f04f4d3d
helter daniel <helterdaniel@hotmail.com>
2021-04-10 21:23:07 -0300

    paginação

--- contatos/views.py
+++ contatos/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http.request import HttpRequest
 from django.shortcuts import get_object_or_404, render
 
@@ -6,8 +7,11 @@ from .models import Contato
 # Create your views here.
 
 
-def index(request):
+def index(request: HttpRequest):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 1)
+    page = request.GET.get("p")
+    contatos = paginator.get_page(page)
     context = {}
     context["contatos"] = contatos
     return render(request=request, template_name="contatos/index.html", context=context)
