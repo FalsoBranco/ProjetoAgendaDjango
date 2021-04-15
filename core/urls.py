7fd94a6114e7a613e2996bbbe68013069d3b736c
helter daniel <helterdaniel@hotmail.com>
2021-04-14 22:01:09 -0300

    namespace

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("contatos.urls")),
+    path("", include("contatos.urls", namespace="contatos")),
 ]
