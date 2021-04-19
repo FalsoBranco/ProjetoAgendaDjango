dd40f8eabaa201a0db6af8a2a5f4e19c8c4fdcd4
helter daniel <helterdaniel@hotmail.com>
2021-04-19 20:17:30 -0300

    bug fix

--- core/urls.py
+++ core/urls.py
@@ -20,12 +20,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-estudos = True
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("contatos.urls", namespace="contatos")),
 ]
 
-if settings.DEBUG and not estudos:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
